665895434fcc8a4916ac489c3ad882b5f0a66527
A-Farabi <farabisyl2017@gmail.com>
2025-07-27 17:51:00 +0600

    succesfully sending add arti data in client

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const cors = require('cors')
 const app = express();
 require('dotenv').config()
 app.use(cors())
+app.use(express.json());
 const port = process.env.PORT || 5000
 
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { default: axios } = require('axios');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mrtaf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -48,6 +50,12 @@ app.get('/all-artifacts', async(req, res) =>{
 app.get('/all-artifacts/:id', async(req, res)=>{
   const artifactId = req.params.id
   const result =await artifactsCollection.findOne(new ObjectId(artifactId))
+  res.send(result)
+})
+
+app.post('/add-artifacts', async(req, res)=>{
+  const newArtifact = req.body
+  const result = await artifactsCollection.insertOne(newArtifact)
   res.send(result)
 })
 
